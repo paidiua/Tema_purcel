709f1dd6b6f46d2981b926ac6717f6bea431952b
george.grigorescu <george.grigorescu@netcentric.biz>
2020-01-18 20:11:18 +0200

    add cat exceptions

---
+++ src/main/java/buc13Exception/CatCantCompileException.java
@@ -0,0 +1,7 @@
+package buc13Exception;
+
+public class CatCantCompileException extends Exception {
+    public CatCantCompileException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/buc13Exception/InvalidCatException.java
@@ -0,0 +1,8 @@
+package buc13Exception;
+
+public class InvalidCatException extends RuntimeException {
+
+    public InvalidCatException(String message) {
+        super(message);
+    }
+}
